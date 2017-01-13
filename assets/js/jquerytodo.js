67614c22fadfa81f240ca3bf9f1ae1bc186b565b
ksamualtolch <ksamualtolch@gmail.com>
2017-01-13 01:08:19 -0800

    Began coding basic list

---
+++ assets/js/jquerytodo.js
@@ -0,0 +1,44 @@
+console.log("goooood evening");
+
+
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+	//or you could use this longer logic
+		// //if li is grey
+		// if($(this).css("color") === "rgb(128, 128, 128)"){
+		// 	//turn it black,
+		// 	$(this).css({
+		// 	color: "black",
+		// 	textDecoration: "none"
+		// });
+		// }
+		// //else
+		// else {
+		// 	//turn it gray
+		// 	$(this).css({
+		// 	color: "gray",
+		// 	textDecoration: "line-through"
+		// });
+		// }
+});
+
+//click on x to delete todo
+
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13){
+		//grabbing new text from input
+		var todoText = $(this).val();	
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+
+	}
+})
